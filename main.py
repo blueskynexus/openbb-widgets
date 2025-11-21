4849ef34cb08a93e956b853d1679cf0964f82653
smithw94 <smithw1994@gmail.com>
2025-11-21 11:53:25 -0800

    lint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import util.configure_logging as util
 from via_nexus.dataset import stock_stats, vnx_quote
 
+
 def main():
     util.configure_logging()
 
@@ -11,6 +12,7 @@ def main():
     vnx_quote_data = vnx_quote.data(["AAPL"])
     if len(vnx_quote_data) == 0:
         raise ValueError("No data found for the given symbols")
+
 
 if __name__ == "__main__":
     main()
--- via_nexus/dataset.py
+++ via_nexus/dataset.py
@@ -3,6 +3,7 @@ import httpx
 import logging
 from config import settings
 
+
 class Dataset:
     def __init__(self, namespace: str, dataset: str):
         self.base_url = settings.vianexus_base_url
@@ -11,7 +12,7 @@ class Dataset:
         self.dataset = dataset
 
     def make_request(self, symbols: list[str]):
-        '''Make a request to the Vianexus API to get the data for the dataset for the given symbols'''
+        """Make a request to the Vianexus API to get the data for the dataset for the given symbols"""
         url = f"{self.base_url}/data/{self.namespace}/{self.dataset}/{','.join(symbols)}"
         params = {
             "token": self.api_key,
@@ -19,12 +20,13 @@ class Dataset:
         }
         response = httpx.get(url, params=params, timeout=10)
         return response.json()
-    
+
     def data(self, symbols: list[str]):
-        '''Get the data for the dataset for the given symbols'''
+        """Get the data for the dataset for the given symbols"""
         data = self.make_request(symbols)
         logging.debug(f"Data: \n{json.dumps(data, indent=4)}")
         return data
+
 
 stock_stats = Dataset("CORE", "STOCK_STATS_US")
 vnx_quote = Dataset("EDGE", "VNX_QUOTE")
